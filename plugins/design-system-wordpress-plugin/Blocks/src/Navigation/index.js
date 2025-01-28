97b58d724b64d56aab1a79f24bc7053f8eb71d59
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-01-28 09:04:48 -0800

    new baseline!!123

--- plugins/design-system-wordpress-plugin/Blocks/src/Navigation/index.js
+++ plugins/design-system-wordpress-plugin/Blocks/src/Navigation/index.js
@@ -4,7 +4,6 @@ import Edit from './edit';
 import Save from './save';
 import metadata from './block.json';
 import './style.scss';
-
 registerBlockType(metadata.name, {
     edit: Edit,
     save: Save,
