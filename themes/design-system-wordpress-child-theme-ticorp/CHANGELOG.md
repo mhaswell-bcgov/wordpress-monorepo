a65a9e7464d96061aa2641a597f969ff79d7cda4
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-02-07 12:28:43 -0800

    Added Header

--- themes/design-system-wordpress-child-theme-ticorp/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Changelog
-
-## 1.0.0 December 3, 2024
-
--  [DESCW-2811](https://citz-gdx.atlassian.net/browse/DESCW-2811) Intialize the theme 
-
--- themes/design-system-wordpress-child-theme-ticorp/dist/index.asset.php
+++ themes/design-system-wordpress-child-theme-ticorp/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '2423021ee2fabd9857a8');
+<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
