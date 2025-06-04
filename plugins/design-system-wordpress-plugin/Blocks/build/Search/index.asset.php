f8d5b31782849f8dff994d4e993d54df3be7b56c
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-06-04 15:27:55 -0700

    removed search

--- plugins/design-system-wordpress-plugin/Blocks/build/Search/index.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return array(
-	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks' ),
-	'version'      => 'b22f4085f1e6674d0df3',
-);
--- plugins/design-system-wordpress-plugin/Blocks/build/Search/view.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return array(
-	'dependencies' => array(),
-	'version'      => 'e83cd1a53752922ac355',
-);
