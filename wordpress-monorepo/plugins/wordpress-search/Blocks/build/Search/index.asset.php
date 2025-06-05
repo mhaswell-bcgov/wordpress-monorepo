f059ec50a3aca28129176d849aaebfd580dd0aaa
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-06-05 16:11:54 -0700

    fran phpcs

--- wordpress-monorepo/plugins/wordpress-search/Blocks/build/Search/index.asset.php
+++ wordpress-monorepo/plugins/wordpress-search/Blocks/build/Search/index.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'de77a1454ea4d3fb7776');
+<?php return array(
+	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks' ),
+	'version'      => 'de77a1454ea4d3fb7776',
+);
