6d28d919c6e0ef73357b884897738c01f5a84610
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-06-27 13:17:52 -0700

    updated styles and functionality to match the wireframe

--- wordpress-monorepo/plugins/wordpress-search/Blocks/build/SearchMetadataFilter/index.asset.php
+++ wordpress-monorepo/plugins/wordpress-search/Blocks/build/SearchMetadataFilter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2607652a630b4c61e93d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fd246206280a5f67efce');
--- wordpress-monorepo/plugins/wordpress-search/Blocks/build/SearchMetadataFilter/view.asset.php
+++ wordpress-monorepo/plugins/wordpress-search/Blocks/build/SearchMetadataFilter/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '7e0431c07e4c643f436e');
+<?php return array('dependencies' => array(), 'version' => 'fba37f8b3832f5aaa1c5');
