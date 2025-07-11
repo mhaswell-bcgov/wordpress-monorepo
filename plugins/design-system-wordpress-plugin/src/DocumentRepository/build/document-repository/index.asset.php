37eb7bb8540549f4e8ca4496e7f3f8db7332ae0f
Kyle Shapka <Kyle.Shapka@gov.bc.ca>
2025-07-11 14:53:47 -0700

    DSWP-225 Remove Document Repository functionality

--- plugins/design-system-wordpress-plugin/src/DocumentRepository/build/document-repository/index.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return array(
-	'dependencies' => array( 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n' ),
-	'version'      => 'c7ec468a88264bfeeece',
-);
--- plugins/design-system-wordpress-plugin/src/DocumentRepository/build/metadata-settings/index.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return array(
-	'dependencies' => array( 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives' ),
-	'version'      => 'f6cf6ba5f9953cbe061f',
-);
