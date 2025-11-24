e33376b467c2198dbababcf18aa60cbf99d7e23c
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-11-24 14:32:52 -0800

    DSWP-283 match style of DSWP plugin to the DSWP theme
    - scaffold out empty markdown files for future documentation
    - add folder structure to match theme documentation
    - configure vuepress to match theme's config
    - borrow index.scss from the DSWP theme styles folder

--- plugins/design-system-wordpress-plugin/documentation/docs/.vuepress/config.js
+++ plugins/design-system-wordpress-plugin/documentation/docs/.vuepress/config.js
@@ -1,49 +1,76 @@
-import { defaultTheme } from '@vuepress/theme-default';
+import { defaultTheme } from "@vuepress/theme-default";
 
-import { defineUserConfig } from 'vuepress';
-import { searchPlugin } from '@vuepress/plugin-search'
-import { viteBundler } from '@vuepress/bundler-vite';
+import { defineUserConfig } from "vuepress";
+import { searchPlugin } from "@vuepress/plugin-search";
+import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
-    base: '/design-system-wordpress-plugin/',
-    lang: 'en-US',
-    title: 'Design System WordPress Plugin',
-    description: 'Developer Documentation for Design System WordPress Plugin',
-    bundler: viteBundler({}),
-    theme: defaultTheme({
-        logo: '/images/BCID_H_rgb_pos.png',
-        logoDark: '/images/BCID_H_rgb_rev.png',
-        editLink: false,
-        lastUpdated: false,
-        repo: 'bcgov/design-system-wordpress-plugin',
-        repoLabel: 'Github',
-        sidebarDepth: 2,  // 
-        navbar: [
-            {
-                text: 'Home',
-                link: '/',
-            },
-        ],
-        sidebar: [
-            {
-                text: 'Site Editor',
-                collapsible: true,
-                children: [],
-            },
-            {
-                text: 'Developers',
-                collapsible: true,
-                children: []
-            }
-        ],
-    }),
-    plugins: [
-        searchPlugin({
-          locales: {
-            '/': {
-              placeholder: 'Search...',
-            },
-          },
-        }),
-      ],
+	base: "/design-system-wordpress-plugin/",
+	lang: "en-US",
+	title: "Design System WordPress Plugin",
+	description: "Developer Documentation for Design System WordPress Plugin",
+	bundler: viteBundler({}),
+	theme: defaultTheme({
+		logo: "/images/BCID_H_rgb_pos.png",
+		logoDark: "/images/BCID_H_rgb_rev.png",
+		editLink: false,
+		lastUpdated: false,
+		repo: "bcgov/design-system-wordpress-plugin",
+		repoLabel: "Github",
+		sidebarDepth: 2,
+		navbar: [
+			{
+				text: "Home",
+				link: "/",
+			},
+		],
+		sidebar: [
+			{
+				text: "Site Editor",
+				collapsible: true,
+				children: [
+					{ text: "CSP", link: "/guide/SiteEditor/CSP" },
+					{
+						text: "Notification Banner",
+						link: "/guide/SiteEditor/NotificationBanner",
+					},
+					{
+						text: "In Page Navigation",
+						link: "/guide/SiteEditor/InPageNavigation",
+					},
+					{
+						text: "Navigation",
+						link: "/guide/SiteEditor/Navigation",
+					},
+					{
+						text: "Auto Anchor",
+						link: "/guide/SiteEditor/AutoAnchor",
+					},
+					{
+						text: "BreadCrumb",
+						link: "/guide/SiteEditor/BreadCrumb",
+					},
+				],
+			},
+			{
+				text: "Developers",
+				collapsible: true,
+				children: [
+					{
+						text: "Unit Test Best Practices",
+						link: "/guide/Developers/UnitTestBestPractices",
+					},
+				],
+			},
+		],
+	}),
+	plugins: [
+		searchPlugin({
+			locales: {
+				"/": {
+					placeholder: "Search...",
+				},
+			},
+		}),
+	],
 });
---
+++ plugins/design-system-wordpress-plugin/documentation/docs/guide/SiteEditor/InPageNavigation.md
@@ -0,0 +1,1 @@
+# In Page Navigation
