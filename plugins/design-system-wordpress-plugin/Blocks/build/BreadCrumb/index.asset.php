117787db80b5e41a658dff2d93313f54713caf35
kshapka-bcgov <kyle.shapka@gov.bc.ca>
2025-11-25 12:11:06 -0800

    Fix breadcrumbs not wrapping

--- plugins/design-system-wordpress-plugin/Blocks/build/BreadCrumb/index.asset.php
+++ plugins/design-system-wordpress-plugin/Blocks/build/BreadCrumb/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n' ),
-	'version'      => '5e4f779356d8f10b3cd8',
+	'version'      => '3bd9096b59afa1e456b8',
 );
--- plugins/design-system-wordpress-plugin/Blocks/build/Navigation/index.asset.php
+++ plugins/design-system-wordpress-plugin/Blocks/build/Navigation/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n' ),
-	'version'      => '64ae097127525e045511',
+	'version'      => 'c97fb2e9c0542f5137e2',
 );
