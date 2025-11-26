7ffcea16bc938d318a2eafc84db9bce353ea53c1
Michael Haswell <Michael.Haswell@gov.bc.ca>
2025-11-26 14:06:02 -0800

    Set temporary pixel tolerance to screenshot tests

--- wordpress-monorepo/plugins/wordpress-search/tests/e2e/Blocks/SearchPostFilter/test.spec.js
+++ wordpress-monorepo/plugins/wordpress-search/tests/e2e/Blocks/SearchPostFilter/test.spec.js
@@ -4,6 +4,9 @@ test.describe('Block', () => {
     const BLOCK_NAME = 'wordpress-search/search-post-type-filter';
     const BLOCK_CLASS = '.wp-block-wordpress-search-search-post-type-filter';
 
+    // TODO: Run e2e tests in Playwright Docker container for consistency.
+    const SCREENSHOT_OPTIONS = {maxDiffPixelRatio: 0.02};
+
     test.beforeEach(async ({ admin }) => {
         // Create a new post before each test
         await admin.createNewPost();
@@ -21,7 +24,7 @@ test.describe('Block', () => {
 
         // Should probably make this into a reusable function as it will appear often.
         const preview = (await editor.openPreviewPage()).locator(BLOCK_CLASS);
-        await expect(preview).toHaveScreenshot();
+        await expect(preview).toHaveScreenshot(SCREENSHOT_OPTIONS);
     });
 
     test('should remove post type', async ({ editor }) => {
@@ -30,7 +33,7 @@ test.describe('Block', () => {
         expect(await editor.getEditedPostContent()).toMatchSnapshot();
 
         const preview = (await editor.openPreviewPage()).locator(BLOCK_CLASS);
-        await expect(preview).toHaveScreenshot();
+        await expect(preview).toHaveScreenshot(SCREENSHOT_OPTIONS);
     });
 
     test('should add back all post types when unselecting all', async ({ editor }) => {
@@ -57,6 +60,6 @@ test.describe('Block', () => {
         // we can't effectively screenshot it.
 
         // const preview = (await editor.openPreviewPage()).getByRole('main');
-        // await expect(preview).toHaveScreenshot();
+        // await expect(preview).toHaveScreenshot(SCREENSHOT_OPTIONS);
     });
 });
