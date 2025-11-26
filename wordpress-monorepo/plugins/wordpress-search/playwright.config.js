19e58aee4164fe1ad731dc06a8cc6ad36df241db
Michael Haswell <Michael.Haswell@gov.bc.ca>
2025-11-26 08:28:35 -0800

    Set Playwright viewport size explicitly

--- wordpress-monorepo/plugins/wordpress-search/playwright.config.js
+++ wordpress-monorepo/plugins/wordpress-search/playwright.config.js
@@ -4,6 +4,14 @@ import baseConfig from '@wordpress/scripts/config/playwright.config.js';
 const config = defineConfig({
     ...baseConfig,
     testDir: 'tests/e2e',
+    projects: [
+        {
+          name: 'chromium',
+          use: {
+            viewport: {width: 1280, height: 720},
+          },
+        },
+    ],
 });
 
 export default config;
