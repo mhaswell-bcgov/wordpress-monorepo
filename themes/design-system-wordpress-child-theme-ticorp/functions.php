6413e16247d52c4936b5b34ba5042d3ef04d4984
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-01-15 13:28:12 -0800

    package lock update for testing. added empty functions.php for trsting

---
+++ themes/design-system-wordpress-child-theme-ticorp/functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package Design_System_WordPress_Child_Theme_TI_Corp
+ */
+
+// This file is required for WordPress theme detection and PHPUnit testing.
