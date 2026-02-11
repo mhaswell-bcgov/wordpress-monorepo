cdb52242952b3e747fcdef5fbd0c7bdcf87bb131
Michael Haswell <Michael.Haswell@gov.bc.ca>
2026-02-11 11:43:26 -0800

    Restrict playwright to running on Chromium Remove workflow step to install browsers on host machine Add missing composer.lock

--- packages/e2e/playwright.config.js
+++ packages/e2e/playwright.config.js
@@ -3,6 +3,12 @@ import baseConfig from '@wordpress/scripts/config/playwright.config.js';
 
 const config = defineConfig( {
     ...baseConfig,
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+    ],
     testDir: 'tests/e2e',
 } );
 
