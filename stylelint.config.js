85f23a5285919ac60f8596975124c22695754ab1
Michael Haswell <Michael.Haswell@gov.bc.ca>
2026-03-20 14:34:16 -0700

    Try modern stylelint config file

---
+++ stylelint.config.js
@@ -0,0 +1,13 @@
+/** @type {import('stylelint').Config} */
+export default {
+    extends: [ "@wordpress/stylelint-config/scss" ],
+    ignoreFontFamilies: [ "BCSans" ],
+    ignoreFiles: [ "**/dist/**" ],
+    rules: {
+        "selector-class-pattern": null,
+        "no-descending-specificity": null,
+        "scss/selector-no-redundant-nesting-selector": null,
+        "font-weight-notation": "named-where-possible",
+        "selector-id-pattern": null
+    }
+};
