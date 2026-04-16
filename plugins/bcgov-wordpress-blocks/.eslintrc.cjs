f2c505ec97e61446128a69010f6f9f8afb8ffadc
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-04-16 13:40:15 -0700

    remove eslint.rc

--- plugins/bcgov-wordpress-blocks/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/** Gutenberg stack uses React; pin version so eslint-plugin-react does not need a local `react` install. */
-module.exports = {
-    settings: {
-        react: {
-            version: '18.3',
-        },
-    },
-};
