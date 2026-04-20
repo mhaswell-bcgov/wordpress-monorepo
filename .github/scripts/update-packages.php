ee9a19e49d187334a4ad25e919443c15ea71a9be
Michael Haswell <Michael.Haswell@gov.bc.ca>
2026-04-20 15:41:19 -0700

    Fix

--- .github/scripts/update-packages.php
+++ .github/scripts/update-packages.php
@@ -5,6 +5,7 @@ $existingJson = file_exists('packages.json') ? json_decode(file_get_contents('pa
 // These variables would be passed from the GH Action environment
 $repository = getenv('REPOSITORY'); // e.g., "bcgov/wordpress-monorepo"
 $tag = getenv('TAG'); // e.g., "bcgov-wordpress-blocks/v1.2.0"
+$version = getenv('VERSION'); // e.g., "v1.2.0"
 $path = getenv( 'PROJECT_PATH' ); // e.g., 'themes/theme-a' or 'plugins/plugin-b'
 
 // Load the local composer.json for this subproject to get 'require', 'autoload', etc.
@@ -12,12 +13,17 @@ $localMetadata = json_decode(file_get_contents("$path/composer.json"), true);
 
 // Construct the new version entry
 $newEntry = array_merge($localMetadata, [
-    "version" => ltrim($version, 'v'),
+    "version" => $version,
     "dist" => [
         "url" => "https://github.com/$repository/releases/download/$tag/dist.zip",
         "type" => "zip"
     ]
 ]);
+
+// Remove unnecessary properties before merging into packages
+unset($newEntry['repositories']);
+unset($newEntry['version']);
+unset($newEntry['scripts']);
 
 // Update the master array
 $existingJson['packages'][$newEntry['name']][$version] = $newEntry;
