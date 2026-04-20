ccd3f688214c5963415e68c0764c6440331f2c33
Michael Haswell <Michael.Haswell@gov.bc.ca>
2026-04-20 15:34:43 -0700

    Fix

--- .github/scripts/update-packages.php
+++ .github/scripts/update-packages.php
@@ -3,8 +3,8 @@
 $existingJson = file_exists('packages.json') ? json_decode(file_get_contents('packages.json'), true) : ['packages' => []];
 
 // These variables would be passed from the GH Action environment
-$subprojectName = getenv('SUBPROJECT'); // e.g., "bcgov-wordpress-blocks"
-$version = getenv('VERSION'); // e.g., "v1.2.0"
+$repository = getenv('REPOSITORY'); // e.g., "bcgov/wordpress-monorepo"
+$tag = getenv('TAG'); // e.g., "bcgov-wordpress-blocks/v1.2.0"
 $path = getenv( 'PROJECT_PATH' ); // e.g., 'themes/theme-a' or 'plugins/plugin-b'
 
 // Load the local composer.json for this subproject to get 'require', 'autoload', etc.
@@ -14,7 +14,7 @@ $localMetadata = json_decode(file_get_contents("$path/composer.json"), true);
 $newEntry = array_merge($localMetadata, [
     "version" => ltrim($version, 'v'),
     "dist" => [
-        "url" => "https://github.com/bcgov/wordpress-monorepo/releases/download/$subprojectName/$version/dist.zip",
+        "url" => "https://github.com/$repository/releases/download/$tag/dist.zip",
         "type" => "zip"
     ]
 ]);
