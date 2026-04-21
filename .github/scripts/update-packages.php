4057caf59f06c0b2cbd7e016036e6f05b47a70c7
Michael Haswell <Michael.Haswell@gov.bc.ca>
2026-04-21 16:10:55 -0700

    Improve update-packages to add useful metadata Remove commit step as it's unnecessary

--- .github/scripts/update-packages.php
+++ .github/scripts/update-packages.php
@@ -6,6 +6,7 @@ $existingJson = file_exists('packages.json') ? json_decode(file_get_contents('pa
 $repository = getenv('REPOSITORY'); // e.g., "bcgov/wordpress-monorepo"
 $tag = getenv('TAG'); // e.g., "bcgov-wordpress-blocks/v1.2.0"
 $version = getenv('VERSION'); // e.g., "v1.2.0"
+$normalizedVersion = ltrim($version, 'v'); // e.g., "1.2.0"
 $path = getenv( 'PROJECT_PATH' ); // e.g., 'themes/theme-a' or 'plugins/plugin-b'
 
 // Load the local composer.json for this subproject to get 'require', 'autoload', etc.
@@ -13,19 +14,26 @@ $localMetadata = json_decode(file_get_contents("$path/composer.json"), true);
 
 // Construct the new version entry
 $newEntry = array_merge($localMetadata, [
-    "version" => $version,
+    "version" => $normalizedVersion,
     "dist" => [
         "url" => "https://github.com/$repository/releases/download/$tag/dist.zip",
-        "type" => "zip"
+        "type" => "zip",
+        "reference" => $tag
+    ],
+    "source" => [
+        "type" => "git",
+        "url" => "https://github.com/$repository.git",
+        "reference" => $tag
     ]
 ]);
 
 // Remove unnecessary properties before merging into packages
 unset($newEntry['repositories']);
+unset($newEntry['require-dev']);
 unset($newEntry['scripts']);
 
 // Update the master array
-$existingJson['packages'][$newEntry['name']][$version] = $newEntry;
+$existingJson['packages'][$newEntry['name']][$normalizedVersion] = $newEntry;
 
 // Save back
 file_put_contents('public/packages.json', json_encode($existingJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
