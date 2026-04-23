0da149329e3a757a2be8b703098211f8d9a6cb53
Michael Haswell <Michael.Haswell@gov.bc.ca>
2026-04-23 10:21:43 -0700

    Improve tag parsing and archive creation in tag.yml Update update-packages script to add more composer metadata

--- .github/scripts/update-packages.php
+++ .github/scripts/update-packages.php
@@ -3,24 +3,37 @@
 $existingJson = file_exists('packages.json') ? json_decode(file_get_contents('packages.json'), true) : ['packages' => []];
 
 // These variables would be passed from the GH Action environment
-$subprojectName = getenv('SUBPROJECT'); // e.g., "bcgov-wordpress-blocks"
+$repository = getenv('REPOSITORY'); // e.g., "bcgov/wordpress-monorepo"
+$tag = getenv('TAG'); // e.g., "bcgov-wordpress-blocks/v1.2.0"
 $version = getenv('VERSION'); // e.g., "v1.2.0"
-$type = getenv( 'TYPE' ); // e.g., 'themes' or 'plugins'
+$normalizedVersion = ltrim($version, 'v'); // e.g., "1.2.0"
+$path = getenv( 'PROJECT_PATH' ); // e.g., 'themes/theme-a' or 'plugins/plugin-b'
 
 // Load the local composer.json for this subproject to get 'require', 'autoload', etc.
-$localMetadata = json_decode(file_get_contents("$type/$subprojectName/composer.json"), true);
+$localMetadata = json_decode(file_get_contents("$path/composer.json"), true);
 
 // Construct the new version entry
 $newEntry = array_merge($localMetadata, [
-    "version" => ltrim($version, 'v'),
+    "version" => $normalizedVersion,
     "dist" => [
-        "url" => "https://github.com/bcgov/wordpress-monorepo/releases/download/$subprojectName/$version/dist.zip",
-        "type" => "zip"
+        "url" => "https://github.com/$repository/releases/download/$tag/dist.zip",
+        "type" => "zip",
+        "reference" => $tag
+    ],
+    "source" => [
+        "type" => "git",
+        "url" => "https://github.com/$repository.git",
+        "reference" => $tag
     ]
 ]);
 
+// Remove unnecessary properties before merging into packages
+unset($newEntry['repositories']);
+unset($newEntry['require-dev']);
+unset($newEntry['scripts']);
+
 // Update the master array
-$existingJson['packages'][$packageName][$version] = $newEntry;
+$existingJson['packages'][$newEntry['name']][$normalizedVersion] = $newEntry;
 
 // Save back
 file_put_contents('public/packages.json', json_encode($existingJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
